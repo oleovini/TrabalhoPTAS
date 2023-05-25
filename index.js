bd1d909e1e5123f1c11dc68467a65c32ed514780
oleovini <leonardo.costa3@estudante.ifms.edu.br>
2023-05-25 07:31:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
-const { Pessoa } = require('./models');
+const { pessoa } = require('./models');
 
 const app = express();
 
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -12,33 +13,37 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/pessoas', async function(req, res){
-  var pessoas = await Pessoa.findAll();
+  var pessoas = await pessoa.findAll();
   res.render('index', { pessoas });
 })
 
 app.get('/pessoas/adicionar', async function(req, res){
-  res.render('pessoas/adicionar');
+  var pessoas = await pessoa.findAll();
+  res.render('pessoas/adicionar', { pessoas });
 })
 
 app.post('/pessoas/adicionar', async function(req, res){
   try {
       await pessoa.create(req.body);
       res.redirect('/pessoas')
+      console.log(req.body)
   } catch (err) {
       console.error(err);
-      res.status(500).json({ message: 'Ocorreu um erro ao adicionar o pessoa.' });
+      res.status(500).json({ message: 'Ocorreu um erro ao adicionar a pessoa.' });
   }
 })
 
-app.post('/pessoas/deletar', async function (req, res) {
+
+app.get('/pessoas/deletar', async function(req, res){
   try {
-    await pessoa.destroy({ where: { id: req.query.id } });
-    res.redirect('/pessoas');
+      await pessoa.destroy({where: {id: req.query.id}});
+      res.redirect('/pessoas')
+      
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Ocorreu um erro ao excluir o usuário.' });
+      console.error(err);
+      res.status(500).json({ message: 'Ocorreu um erro ao excluir a pessoa.' });
   }
-});
+})
 
 app.listen(3000, function() {
   console.log('App de Exemplo escutando na porta 3000!')
